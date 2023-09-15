805d37774a978147dddf7b0b521b7f629003b85a
SandroNebieridze <nebsandro@gmail.com>
2023-09-15 15:34:20 +0400

    sad

---
+++ index.js
@@ -0,0 +1,36 @@
+//  დავალება 1 ვერ გავიგე
+
+// დავალება 2
+
+function Sum(n1,n2,n3,n4,n5,n6,n7,n8,n9){
+    return n1+n2+n3+n4+n5+n6+n7+n8+n9
+}
+let result = Sum(10, 50, 6, 7, 8, 11, 6, 3, 9)
+
+console.log(result);
+
+
+
+
+// დავალება 3
+
+let number = prompt("enter number: ") 
+    if(number % 2 > 0) {
+        console.log("The number is odd")
+       
+    }
+    else {
+        console.log("The number is even");
+};
+
+
+
+
+
+
+
+
+
+
+ 
+
